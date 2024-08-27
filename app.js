87551b029d6797922bafd712ad82e7d1b8830eba
Mohammad Rakib <mohammadrakib224@gmail.com>
2024-08-27 20:38:33 +0600

    Applying Main Logic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let modeChangeButton=document.querySelector("#modeChangeButton");
 let body=document.querySelector("body");
 let choices=document.querySelector(".choices");
+let choiceRPS=document.querySelectorAll(".choice");
+
 let currentMode="Light";
 
 modeChangeButton.addEventListener("click",function(){
@@ -23,3 +25,68 @@ else{
 
 }
 });
+
+choiceRPS.forEach((choice) => {
+    choice.addEventListener("click",() =>{
+        const userChoice=choice.getAttribute("id");
+        printGameCalculation(userChoice);
+        
+    });
+});
+
+const genCompChoice= () => {
+let optionC=["rock","paper","scissors"];
+const randomID=Math.floor(Math.random()*3);
+return optionC[randomID];
+}
+    
+const printGameCalculation=(userChoice)=>{
+        let compChoice= genCompChoice();
+        console.log("User choice = ",userChoice);
+        console.log("Computer Choice = ",compChoice);
+
+        if(compChoice==userChoice){
+console.log("Game Draw");
+
+        }
+        else{
+            let userWin =true;
+            if(userChoice=="rock"){
+                userWin=compChoice=="scissors"?true:false;
+            }
+            else if(userChoice=="paper"){
+                userWin=compChoice=="rock"?true:false;
+            }
+            else if(userChoice=="scissors"){
+                userWin=compChoice=="rock"?false:true;
+            }
+                printWinnner(userWin);
+        }}
+
+
+        const printWinnner=(userWin)=>{
+
+            if(userWin==true){
+                console.log("You Win ! ");
+            }
+            else{
+                console.log("Computer Win ! ");
+            }}
+    
+
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+
+
+
+    
+
